eedd8d67c88c7eced9edc7ee7846fa3606cd7d21
shajanthan <shajanthan@codegen.net>
2023-09-26 00:27:48 +0530

    Added animations and transitions to Header component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Home/>
     </div>
   );
 }
---
+++ my-app/src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Navbar from '../../components/Navbar/Navbar';
+
+const Home = () => {
+
+    return(
+        <div>
+            <Header/>
+        </div>
+    )
+};
+
+export default Home;
