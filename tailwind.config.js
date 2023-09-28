711d4626280b49bbf449d04d5145aac8676da3c7
shajanthan <shajanthan@codegen.net>
2023-09-28 10:05:41 +0530

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "light-beige": "#F5EEE8",
+        "dark-brown": "#3A3531",
+        "grayish-brown": "#988E85",
+        "light-gray": "#979591",
+        "dark-gray": "#615D5A",
+        "pale-yellow": "#E4DCCF",
+        "warm-gray": "#8D8178",
+        "slate-gray": "#667085",
+        "black": "#080808",
+        "white": "#FFFFFF",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        k2d: ["K2D", "serif"],
+        khula: ["Khula", "sans-serif"],
+        bebasneue: ["Bebas Neue", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
