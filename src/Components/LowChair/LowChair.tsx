59532c23f571f2b6b80a6802ef399633741b9237
shajanthan <shajanthan@codegen.net>
2023-09-29 13:09:03 +0530

    product infomation sections added

--- src/Components/LowChair/LowChair.tsx
+++ src/Components/LowChair/LowChair.tsx
@@ -27,7 +27,7 @@ const LowChair = () => {
     hidden: { scale: 0 },
     visible: {
       scale: 1,
-      transition: { duration: 0.7, ease: "easeInOut" },
+      transition: { duration: 0.8, ease: "easeInOut" },
     },
   };
 
