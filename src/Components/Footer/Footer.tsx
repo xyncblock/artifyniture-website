bbe340bde66aca4f739df79494348e941f5addce
shajanthan <shajanthan@codegen.net>
2023-10-01 20:00:55 +0530

    responsive added for the footer and testimonial componenet

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,16 +2,18 @@ import { FaInstagram, FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <div className="footer bg-light-beige flex flex-row justify-between items-center py-5 px-5">
-        <div className="copyright  text-dark-brown text-base font-light font-inter w-[25rem] whitespace-nowrap">© 2023 ARTIFYNITURE. All rights reserved.</div>
-        <div className="logo text-dark-brown text-xl font-semibold font-k2d w-[25rem] flex justify-center">ARTIFYNITURE</div>
-        <div className="social-media-icons flex flex-row gap-4 items-center w-[25rem] justify-end">
-            <FaFacebook size={20} color="#080808"/>
-            <FaInstagram size={20} color="#080808"/>
-            <FaTwitter size={20} color="#080808"/>
-            <FaYoutube size={20} color="#080808"/>
+    <div className="footer bg-light-beige flex flex-col-reverse sm:flex-row justify-between items-center py-5 px-5 gap-1">
+        <div className="copyright  text-dark-brown text-xs sm:text-xs font-light font-inter sm:w-[21rem] whitespace-nowrap justify-center items-center sm:justify-start">© 2023 ARTIFYNITURE. All rights reserved.</div>
+        <div className="logo text-dark-brown text-lg sm:text-xl font-semibold font-k2d sm:w-[21rem] flex justify-center">ARTIFYNITURE</div>
+        <div className="social-media-icons flex flex-row gap-4 items-center sm:w-[21rem] justify-center sm:justify-end">
+            <FaFacebook  size={16} color="#080808"/>
+            <FaInstagram size={16} color="#080808"/>
+            <FaTwitter size={16} color="#080808"/>
+            <FaYoutube size={16} color="#080808"/>
         </div>
     </div>
+
+    
   )
 }
 
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -20,8 +20,8 @@ const Home = () => {
         <Table />
         <HighChair />
         <LowChair />
-        {/* <Testimonials />
-        <Footer />  */}
+        <Testimonials />
+        <Footer />
       </div>
     </div>
   );
