61bf5935a7c8af1fbcb1a28cbaa0046eb705d842
shajanthan <shajanthan@codegen.net>
2023-10-05 21:14:22 +0530

    .

--- src/Components/ProductIntro/ProductIntro.tsx
+++ src/Components/ProductIntro/ProductIntro.tsx
@@ -3,38 +3,48 @@ import { ArrowLongRightIcon } from "@heroicons/react/24/solid";
 
 const ProductIntro = () => {
   return (
-    <section className="product-intron w-full flex flex-col items-center justify-betweenn py-8 sm:py-20">
-      <div className="text-section flex flex-col items-center justify-between pb-10 sm:pb-18">
-        <div className="text-dark-brown font-medium font-inter text-center
-          text-xl
+    <section className="product-intron w-full flex flex-col items-center justify-betweenn 
+      py-12 px-4
+      sm:py-20">
+      <div className="text-section flex flex-col items-center justify-between 
+        pb-10 
+        sm:pb-18">
+        <div className="text-dark-brown font-medium font-inter text-center items-center
+          text-[1.7rem] leading-9 pb-2
           sm:text-4xl
           md:text-5xl
-          lg:text-6xl
-          pb-[0.16rem] sm:pb-2 
+          lg:text-6xl sm:pb-2 
                   ">
           Introducing Elegance Redefined
         </div>
         <div className="text-center text-light-gray font-normal font-inter
-          text-[0.6rem] max-w-[18rem]
+          text-[0.6rem] 
           sm:text-sm sm:max-w-[36rem]
           md:max-w-[48rem] md:text-xl
-          pb-3 sm:pb-4 
+          pb-6 sm:pb-4 max-w-[17rem]
           ">
           <div>
             We are thrilled to introduce our latest creation - an embodiment of
             meticulous craftsmanship and timeless design
           </div>
         </div>
-        <button className="bg-grayish-brown hover:bg-light-gray flex flex-row justify-center items-center py-[0.35rem] px-3 gap-2 sm:py-2 sm:px-4">
-          <div className="text-[0.6rem] font-normal font-inter tracking-wider text-white sm:text-xs">
+        <button className="bg-grayish-brown hover:bg-light-gray flex flex-row justify-center items-center 
+          py-2 px-4 gap-2 my-3
+          sm:py-2 sm:px-4">
+          <div className="font-normal font-inter tracking-wider text-white 
+            text-[0.6rem] 
+            sm:text-xs">
             Shop The Collection
           </div>
           <div>
-            <ArrowLongRightIcon className=" sm:h-6 sm:w-6 h-4 w-4 text-white" />
+            <ArrowLongRightIcon className="  text-white 
+            h-4 w-4
+            sm:h-6 sm:w-6 " />
           </div>
         </button>
       </div>
-      <div className="image flex justify-center w-full h-full">
+      <div className="image flex justify-center 
+        w-[125%] h-full">
         <img
           src="./Assets/completeSet.jpg"
           alt=""
